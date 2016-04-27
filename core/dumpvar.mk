02495f8d71747e311b74273f4de96750733b59b4
Andres Mora Montero <andresmora1676@gmail.com>
2016-04-27 12:03:04 -0600

    Ulala

--- core/dumpvar.mk
+++ core/dumpvar.mk
@@ -64,7 +64,8 @@ endif # CALLED_FROM_SETUP
 
 ifneq ($(PRINT_BUILD_CONFIG),)
 HOST_OS_EXTRA:=$(shell python -c "import platform; print(platform.platform())")
-$(info ============================================)
+$(info ============== MaxiCM Team presents ==============)
+$(info ========== MaxiCM for $(TARGET_DISPLAY) ==============)
 $(info   PLATFORM_VERSION_CODENAME=$(PLATFORM_VERSION_CODENAME))
 $(info   PLATFORM_VERSION=$(PLATFORM_VERSION))
 $(info   MAXI_VERSION=$(MAXI_VERSION))
